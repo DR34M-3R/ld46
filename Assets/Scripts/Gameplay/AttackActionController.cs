9a02e056d103966162ddfd81c453097721eefe62
vladislav <->
2020-04-19 16:23:27 +0300

    Go animation

---
+++ Assets/Scripts/Gameplay/AttackActionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Gameplay;
+using UnityEngine;
+
+public class AttackActionController : MonoBehaviour
+{
+    public List<Vector3> Positions = new List<Vector3>();
+
+    [SerializeField]
+    private Stats _stats;
+
+    public void Attack()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward);
+
+        if (hit.collider != null)
+        {
+
+        }
+    }
+
+
+ //   private EventSystem GetCollision()
+   // {
+        
+    //}
+    
+    
+}
